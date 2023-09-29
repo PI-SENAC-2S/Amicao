fd0cd21f0d77cbf7369660d17dd6e205141d800c
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2023-09-28 21:47:09 -0300

    Add files via upload

---
+++ Projeto-PI2/src/model/clienteModel.java
@@ -0,0 +1,58 @@
+package model;
+public class clienteModel {
+
+    private int idCliente;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private String cpf;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }    
+}
